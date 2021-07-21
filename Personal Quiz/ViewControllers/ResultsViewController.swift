745d2b23d747449f8d6479397661eb91fa5ea5bf
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-21 23:12:49 +0300

    Initial Commit

---
+++ Personal Quiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultsViewController.swift
+//  Personal Quiz
+//
+//  Created by brubru on 19.07.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultDescription: UILabel!
+    
+    var answers: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        checkAnswers()
+        
+    }
+    
+    private func checkAnswers() {
+        let types = answers.map { $0.type }
+        var frequentTypes = [AnimalType: Int]()
+        types.forEach { frequentTypes[$0] = (frequentTypes[$0] ?? 0) + 1
+        }
+        if let (animal, _) = frequentTypes.max(by: {$0.1 < $1.1}) {
+            resultLabel.text = "Вы - \(animal.rawValue)"
+            resultDescription.text = animal.definition
+        }
+    }
+    
+}
